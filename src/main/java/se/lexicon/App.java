d14d2d49a77742c92dd891b6e917e9348db0197c
AgnesNazie <cheagnesmbo@gmail.com>
2025-03-21 11:39:06 +0100

    tested add method

--- src/main/java/se/lexicon/App.java
+++ src/main/java/se/lexicon/App.java
@@ -7,14 +7,26 @@ public class App {
     public static void main(String[] args) {
         int size = NameRepository.getSize();
         System.out.println(size);
-        NameRepository.setNames(new String[]{"Erik Svensson", "Mehrdad Javan", "Agnes nazie", "Fidelis Fuche"});
+        NameRepository.setNames(new String[]{"Erik Svensson", "Mehrdad Javan"});
         System.out.println(NameRepository.getSize());
-        //declare a variable to store the new names
-        String [] allNames = NameRepository.findAll();
-        //print names.loop through
-        for (String name : allNames) {
-            System.out.println(name);
-        }
         // call more methods as needed
+
+        // Test add() method to add a new name
+        NameRepository.add("Agnes Nazie");
+        System.out.println("Size after adding Agnes Nazie: " + NameRepository.getSize());
+
+
+    }
+
+    // created a Helper method to print names array
+    private static void printNames(String[] names) {
+        if (names.length == 0) {
+            System.out.println("No names found.");
+        } else {
+            for (String name : names) {
+                System.out.println(name);
+            }
+        }
+
     }
 }
